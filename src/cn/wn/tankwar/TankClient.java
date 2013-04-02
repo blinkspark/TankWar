75d0df6fca3b123c5ae4800147ae74d915dea2ff
wandaoxp <196427398@qq.com>
2013-04-02 13:23:31 +0800

    0.0.03 增加键盘监听

--- src/cn/wn/tankwar/TankClient.java
+++ src/cn/wn/tankwar/TankClient.java
@@ -5,6 +5,8 @@ import java.awt.DisplayMode;
 import java.awt.Graphics;
 import java.awt.GraphicsDevice;
 import java.awt.GraphicsEnvironment;
+import java.awt.event.KeyAdapter;
+import java.awt.event.KeyEvent;
 
 import javax.swing.JFrame;
 
@@ -14,10 +16,14 @@ import javax.swing.JFrame;
  *
  */
 public class TankClient extends JFrame {
+	
+	private static final long serialVersionUID = 6432091120610414896L;
 
 	private static final int SCR_HEIGHT = 600;
 	public static final int SCR_WIDTH = 800;
-	private static final long serialVersionUID = 6432091120610414896L;
+	private DisplayMode defaultDisplayMode;
+
+	private GraphicsDevice device;
 
 	/**
 	 * 程序入口
@@ -35,12 +41,12 @@ public class TankClient extends JFrame {
 	 * @param client 要设置全屏的窗口
 	 */
 	private void setFullScreen(TankClient client) {
-		GraphicsDevice device = GraphicsEnvironment
+		device = GraphicsEnvironment
 				.getLocalGraphicsEnvironment().getDefaultScreenDevice();
-		DisplayMode displayMode = device.getDisplayMode();
+		defaultDisplayMode = device.getDisplayMode();
 		device.setFullScreenWindow(client);
 		if (device.isDisplayChangeSupported()) {
-			device.setDisplayMode(new DisplayMode(SCR_WIDTH, SCR_HEIGHT, displayMode
+			device.setDisplayMode(new DisplayMode(SCR_WIDTH, SCR_HEIGHT, defaultDisplayMode
 					.getBitDepth(), DisplayMode.REFRESH_RATE_UNKNOWN));
 		}
 	}
@@ -53,6 +59,7 @@ public class TankClient extends JFrame {
 		setDefaultCloseOperation(EXIT_ON_CLOSE);
 		setSize(SCR_WIDTH, SCR_HEIGHT);
 		setVisible(true);
+		addKeyListener(new GameKeyListener());
 	}
 
 	/**
@@ -72,7 +79,33 @@ public class TankClient extends JFrame {
 		g.fillRect(0, 0, SCR_WIDTH, SCR_HEIGHT);
 		g.setColor(defColor);
 	}
-	
-	
+	/**
+	 * 键盘监听类
+	 * @author Wangning
+	 *
+	 */
+	class GameKeyListener extends KeyAdapter{
+
+		/**
+		 * 重写keyRelease方法监听键盘释放事件
+		 */
+		@Override
+		public void keyReleased(KeyEvent e) {
+			int keyCode = e.getKeyCode();
+			switch (keyCode) {
+			case KeyEvent.VK_ESCAPE:
+				if(device.isDisplayChangeSupported()){
+					device.setDisplayMode(defaultDisplayMode);
+				}
+				device.setFullScreenWindow(null);
+				break;
+
+			default:
+				break;
+			}
+		}
+
+		
+	}
 
 }
