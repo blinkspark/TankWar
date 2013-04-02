bf8f754fcda02c1eb7d9de6a1bb274ebbb5ed948
wandaoxp <196427398@qq.com>
2013-04-02 11:50:22 +0800

    加入注释

--- src/cn/wn/tankwar/TankClient.java
+++ src/cn/wn/tankwar/TankClient.java
@@ -8,12 +8,21 @@ import java.awt.GraphicsEnvironment;
 
 import javax.swing.JFrame;
 
+/**
+ * 主窗口类
+ * @author Wangning
+ *
+ */
 public class TankClient extends JFrame {
 
 	private static final int SCR_HEIGHT = 600;
 	public static final int SCR_WIDTH = 800;
 	private static final long serialVersionUID = 6432091120610414896L;
 
+	/**
+	 * 程序入口
+	 * @param args 命令行参数
+	 */
 	public static void main(String[] args) {
 		TankClient client = new TankClient();
 		client.launchFrame();
@@ -21,6 +30,10 @@ public class TankClient extends JFrame {
 		client.setFullScreen(client);
 	}
 
+	/**
+	 * 设置全屏方法
+	 * @param client 要设置全屏的窗口
+	 */
 	private void setFullScreen(TankClient client) {
 		GraphicsDevice device = GraphicsEnvironment
 				.getLocalGraphicsEnvironment().getDefaultScreenDevice();
@@ -32,6 +45,9 @@ public class TankClient extends JFrame {
 		}
 	}
 
+	/**
+	 * 启动窗口方法
+	 */
 	public void launchFrame() {
 		setTitle("TankWar");
 		setDefaultCloseOperation(EXIT_ON_CLOSE);
@@ -39,11 +55,17 @@ public class TankClient extends JFrame {
 		setVisible(true);
 	}
 
+	/**
+	 * 绘图方法
+	 */
 	@Override
 	public void paint(Graphics g) {
 		backGroundLayer(g);
 	}
-
+	/**
+	 * 背景图层
+	 * @param g 窗口的绘图
+	 */
 	private void backGroundLayer(Graphics g) {
 		Color defColor = g.getColor();
 		g.setColor(Color.GREEN);
